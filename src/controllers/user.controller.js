ad9621c22e780057f4fa61976645bbbe455649c0
Facundo Giuricich <facundogiuricich936@gmail.com>
2025-09-16 22:47:16 -0300

    Desarrollo de controlador - ruta

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { get } from "mongoose";
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import { json } from "sequelize";
 
 const userController = {
   createUser: async (req, res) => {
@@ -51,5 +52,66 @@ const userController = {
       });
     }
   },
+
+   getUserById: async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id).select("-password");
+      if(!user){
+        return res.status(404).json({msg:'usuario no encontrado'});
+      }
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({
+        ok: false,
+        msg: "error al traer el usuario",
+        error: error.message,
+      });
+    }
+  },
+
+  updateUser: async (req, res) => {
+    try {
+      const {id} = req.params; 
+      const{username,email,password,role} = req.body;
+      const updates = {};
+      if(username)updates.username=username;
+      if(email)updates.email=email;
+      if(role)updates.role=role;
+      if(password){
+        const saltRouds = 10;
+      updates.password = await bcrypt.hash(password, saltRouds);
+      }
+      const updatedUser = await User.findByIdAndUpdate(id,updates,{new:true,runValidators:true}).select('-password');
+      if(!updatedUser){
+        return res.status(404).json({msg:'usuario no encontrado'});
+      }
+      res.status(200).json({msg:'usuario actualizado correctamente',user:updatedUser});
+    } catch (error) {
+      res.status(500).json({
+        ok: false,
+        msg: "error al actualizar el usuario",
+        error: error.message,
+      });
+    }
+  },
+
+  deleteUser: async (req, res) => {
+    try {
+      const {id} = req.params; 
+      const user = await User.findById(id);
+      if(!user){
+        return res.status(404).json({msg:'usuario no encontrado'});
+      }
+      user.deleted_at = new Date();
+      await user.save();
+      res.status(200).json({msg:'usuario eliminado correctamente'});
+    } catch (error) {
+      res.status(500).json({
+        ok: false,
+        msg: "error al eliminar el usuario",
+        error: error.message,
+      });
+    }
+  }
 };
 export default userController;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,9 @@ import userController from "../controllers/user.controller.js";
 
 const router = Router();
 
-router.get("all", userController.getAllUsers);
+router.get("/", userController.getAllUsers);
 router.post("/", userController.createUser);
+router.get('/:id',userController.getUserById);
+router.put('/:id',userController.updateUser);
+router.delete('/:id',userController.deleteUser);
 export default router;
